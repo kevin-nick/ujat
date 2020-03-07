7efe9d7648c68b9433e8d0d184b65cc236a8212a
kevin nick <61859405+kevin-nick@users.noreply.github.com>
2020-03-07 00:21:18 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Conway_s_Game_of_Life
+{
+    partial class Conways_Game_of_Life
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Picture_Life = new System.Windows.Forms.PictureBox();
+            this.StartB = new System.Windows.Forms.Button();
+            this.PauseB = new System.Windows.Forms.Button();
+            this.RANDOM = new System.Windows.Forms.Button();
+            this.Tiempo = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Picture_Life)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Picture_Life
+            // 
+            this.Picture_Life.BackColor = System.Drawing.Color.Transparent;
+            this.Picture_Life.Location = new System.Drawing.Point(12, 12);
+            this.Picture_Life.Name = "Picture_Life";
+            this.Picture_Life.Size = new System.Drawing.Size(350, 350);
+            this.Picture_Life.TabIndex = 0;
+            this.Picture_Life.TabStop = false;
+            // 
+            // StartB
+            // 
+            this.StartB.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.StartB.Location = new System.Drawing.Point(278, 382);
+            this.StartB.Name = "StartB";
+            this.StartB.Size = new System.Drawing.Size(84, 29);
+            this.StartB.TabIndex = 1;
+            this.StartB.Text = "START";
+            this.StartB.UseVisualStyleBackColor = false;
+            this.StartB.Click += new System.EventHandler(this.StartB_Click);
+            // 
+            // PauseB
+            // 
+            this.PauseB.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.PauseB.Location = new System.Drawing.Point(182, 382);
+            this.PauseB.Name = "PauseB";
+            this.PauseB.Size = new System.Drawing.Size(79, 29);
+            this.PauseB.TabIndex = 2;
+            this.PauseB.Text = "PAUSE";
+            this.PauseB.UseVisualStyleBackColor = false;
+            this.PauseB.Click += new System.EventHandler(this.PauseB_Click);
+            // 
+            // RANDOM
+            // 
+            this.RANDOM.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.RANDOM.Location = new System.Drawing.Point(12, 382);
+            this.RANDOM.Name = "RANDOM";
+            this.RANDOM.Size = new System.Drawing.Size(84, 29);
+            this.RANDOM.TabIndex = 3;
+            this.RANDOM.Text = "Random";
+            this.RANDOM.UseVisualStyleBackColor = false;
+            this.RANDOM.Click += new System.EventHandler(this.RANDOM_Click);
+            // 
+            // Tiempo
+            // 
+            this.Tiempo.Tick += new System.EventHandler(this.Tiempo_Tick);
+            // 
+            // Conways_Game_of_Life
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 422);
+            this.Controls.Add(this.RANDOM);
+            this.Controls.Add(this.PauseB);
+            this.Controls.Add(this.StartB);
+            this.Controls.Add(this.Picture_Life);
+            this.Name = "Conways_Game_of_Life";
+            this.Text = "Conway\'s Game of Life";
+            ((System.ComponentModel.ISupportInitialize)(this.Picture_Life)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Picture_Life;
+        private System.Windows.Forms.Button StartB;
+        private System.Windows.Forms.Button PauseB;
+        private System.Windows.Forms.Button RANDOM;
+        private System.Windows.Forms.Timer Tiempo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conway_s_Game_of_Life
+{
+    public partial class Conways_Game_of_Life : Form
+    {
+        private int longUniv = 35;
+        private int Tpixel = 10;
+        int[,] celulas;
+    
+        public Conways_Game_of_Life()
+        {
+            InitializeComponent();
+            celulas = new int[longUniv,longUniv];
+        }
+        public void PintarPixel(Bitmap bmp,int x,int y,Color color)
+        {
+            for (int i = 0; i < Tpixel; i++ )
+                for (int j = 0; j < Tpixel; j++)
+                {
+                    bmp.SetPixel(i + (x * Tpixel) , j + (y * Tpixel) , color);
+                }
+           
+        }
+
+        public void pintarCel()
+        {
+            Bitmap bmp = new Bitmap(Picture_Life.Width,Picture_Life.Height);
+
+            for (int i = 0; i < longUniv; i++)
+                for (int j = 0; j < longUniv; j++)
+                {
+                    if (celulas[i, j] == 0)
+                        PintarPixel(bmp, i, j, Color.White);
+                    else
+                        PintarPixel(bmp,i,j,Color.Black);
+                }
+            Picture_Life.Image = bmp;
+        }
+
+        public void juego_vida()
+        {
+            int[,] celulaTemp = new int[longUniv,longUniv];
+            for (int x = 0; x < longUniv; x++)
+                for (int y = 0; y < longUniv; y++)
+                {
+                    if (celulas[x, y] == 0)
+                       celulaTemp[x,y]= Reglas(x, y, false);
+                    else
+                       celulaTemp[x, y] = Reglas(x, y, true);
+                }
+            celulas = celulaTemp;
+        }
+
+        public int Reglas(int x,int y,bool Estado)
+        {
+            int VecinasVivas = 0;
+            if (x > 0 && y > 0)
+                if (celulas[x - 1, y - 1] == 1)
+                    VecinasVivas++;
+            if (y > 0)
+                if (celulas[x, y - 1] == 1)
+                    VecinasVivas++;
+            if (x > longUniv-1 && y > 0)
+                if (celulas[x + 1, y - 1] == 1)
+                    VecinasVivas++;
+            if (x > 0)
+                if (celulas[x - 1, y] == 1)
+                    VecinasVivas++;
+            if (x < longUniv - 1)
+                if (celulas[x + 1, y ] == 1)
+                    VecinasVivas++;
+            if (x > 0 && y < longUniv - 1)
+                if (celulas[x - 1, y + 1] == 1)
+                    VecinasVivas++;
+            if (y < longUniv - 1)
+                if (celulas[x, y + 1] == 1)
+                    VecinasVivas++;
+            if (x < longUniv - 1 && y < longUniv - 1)
+                if (celulas[x + 1, y + 1] == 1)
+                    VecinasVivas++;
+            if (Estado)
+                return (VecinasVivas == 2 || VecinasVivas == 3) ? 1 : 0;
+            else
+                return VecinasVivas == 3 ? 1 : 0;
+        }
+        private void RANDOM_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            for (int x = 0; x < longUniv; x++)
+                for (int y = 0; y < longUniv; y++)
+                    celulas[x, y] = r.Next(0, 2);
+            pintarCel();
+        }
+
+        private void PauseB_Click(object sender, EventArgs e)
+        {
+            Tiempo.Enabled = false;
+        }
+
+        private void StartB_Click(object sender, EventArgs e)
+        {
+            Tiempo.Enabled = true; 
+        }
+
+        private void Tiempo_Tick(object sender, EventArgs e)
+        {
+            juego_vida();
+            pintarCel();
+        }
+    }
+}
